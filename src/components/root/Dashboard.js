f7a952cd23f4d04b647aaec62bb6af35c27a6180
Afkborn <kalaybilgehan60@gmail.com>
2022-09-02 23:51:50 +0300

    dashboard product design demo

--- src/components/root/Dashboard.js
+++ src/components/root/Dashboard.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  Row,
+  Col,
+} from "reactstrap";
 class Dashboard extends Component {
   returnIndex = () => {
     return (
@@ -12,19 +18,36 @@ class Dashboard extends Component {
 
   returnRenderCategory = () => {
     return (
-      <div>
-        <h1>{this.props.currentCategory.name}</h1>
-        {this.props.products.map((product) => (
-          <div key={product.id}>{product.name}</div>
-        ))}
+      <div className="products mt-4">
+        <Row>
+          {this.props.products.map((product) => (
+            <Col md={4} lg={4} sm={4} xs={12} key={product.id} className="menu-item">
+              <Card style={{}}>
+                <img src={product.img} />
+                <CardBody>
+                  <CardTitle className="text-center" tag="h4">
+                    {product.name}
+                  </CardTitle>
+                  <CardTitle className="text-center text-muted" tag="h5">
+                    {product.price} TL
+                  </CardTitle>
+                </CardBody>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </div>
     );
   };
 
   render() {
-    return <div>
-      {this.props.currentCategory.name ? this.returnRenderCategory() : this.returnIndex()}
-    </div>;
+    return (
+      <div>
+        {this.props.currentCategory.name
+          ? this.returnRenderCategory()
+          : this.returnIndex()}
+      </div>
+    );
   }
 }
 
@@ -32,7 +55,7 @@ function mapStateToProps(state) {
   return {
     categories: state.categoryReducer,
     currentCategory: state.changeCategoryReducer,
-    products: state.productReducer
+    products: state.productReducer,
   };
 }
 
